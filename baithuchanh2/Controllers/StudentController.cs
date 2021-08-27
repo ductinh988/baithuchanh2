e50080efa688cdec48b1c0ce97ab9c28f71d8eef
ducvu <vuduc2701@gmail.com>
2021-08-27 14:58:45 +0700

    Add project files.

---
+++ baithuchanh2/Controllers/StudentController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace baithuchanh2.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Getinfo()
+        {
+            return View();
+        }
+       [HttpPost]
+       public ActionResult Getinfo(string ten,string tuoi,string lop)
+        {
+            ViewBag.Ten = ten;
+            ViewBag.Tuoi = tuoi;
+            ViewBag.Lop = lop;
+            return View();
+        }    
+    }
+}
